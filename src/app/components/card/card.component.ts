57797bfe1fb38a73854304e56cf784e6cffb959c
renan-augusto <renan_acm1992@hotmail.com>
2023-09-21 09:01:09 -0300

    desafio concluido

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+  @Input()
+  gameType : string = "";
+  @Input()
+  gamePrice : string = "";
+  @Input() 
+  gameLabel: string = "";
+  @Input()
+  gameCover : string = ""; 
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
